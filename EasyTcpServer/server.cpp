76095a4509ae0904baca140d6e66430804671fa5
spider <13953028063@139.com>
2019-11-19 14:55:34 +0800

    服务器增加select网络模型

--- EasyTcpServer/server.cpp
+++ EasyTcpServer/server.cpp
@@ -7,6 +7,7 @@
 #include <WS2tcpip.h>
 #include <iostream>
 #include <string.h>
+#include <vector>
 //#include "OSInfo.h"
 
 
@@ -65,7 +66,56 @@ struct LogOutResult: public DataHeader
 	int result;
 };
 
+std::vector<SOCKET> g_clients;
 
+int processor(SOCKET accsock)
+{
+	char msgbuf[] = "Hello,I'm Server.";
+	DataHeader dh{};
+	int leng = sizeof(DataHeader);
+	memset(&dh, 0, sizeof(DataHeader));
+	//5 接受客户端的请求
+	int nlen = recv(accsock, (char*)&dh, sizeof(DataHeader), 0);
+	std::cout << "debug:接受到的指令: " << dh.cmd
+		<< "长度:" << dh.dataLength << std::endl;
+	if (nlen <= 0)
+	{
+		std::cout << "客户端关闭" << std::endl;
+		return -1;
+	}
+	switch (dh.cmd)
+	{
+	case CMD_LOGIN:
+	{
+		Login login{};
+		recv(accsock, (char*)&login + leng, sizeof(Login) - leng, 0);
+
+		std::cout << "用户名：" << login.userName << "\n"
+			<< "密码：" << login.passWord << std::endl;
+
+		LoginResult logret{};
+		send(accsock, (char*)&logret, sizeof(logret), 0);
+	}
+	break;
+	case CMD_LOGOUT:
+	{
+		LogOut logout{};
+		recv(accsock, (char*)&logout + leng, sizeof(LogOut) - leng, 0);
+
+		std::cout << "用户 " << logout.userName << " 登出" << std::endl;
+		//忽略判断用户名密码
+		LogOutResult logret{  };
+		send(accsock, (char*)&logret, sizeof(logret), 0);
+	}
+	break;
+	default:
+		dh.cmd = CMD_ERROR;
+		dh.dataLength = 0;
+		std::cout << "不能解析的指令" << std::endl;
+		break;
+	}
+	return 1;
+}
 
 int main()
 {
@@ -109,64 +159,68 @@ int main()
 	//	4. 等待接受客户端连接 ——accept
 	sockaddr_in _clientAddr;
 	int len = sizeof(_clientAddr);
-	char msgbuf[] = "Hello,I'm Server.";
-	DataHeader dh{};
-	int leng = sizeof(DataHeader);
 
+	
+
+	fd_set fdread;
+	fd_set fdwrite;
+	fd_set fdexce;
+
+	SOCKET accsock;
 	while (true)
 	{
-		SOCKET accsock = accept(_sock, (sockaddr*)&_clientAddr, &len);
-		if (INVALID_SOCKET == accsock)
+
+		FD_ZERO(&fdread);
+		FD_ZERO(&fdwrite);
+		FD_ZERO(&fdexce);
+
+		FD_SET(_sock,&fdread);
+		FD_SET(_sock, &fdwrite);
+		FD_SET(_sock, &fdexce);
+
+		for (size_t i = 0; i < g_clients.size(); i++)
 		{
-			std::cout << "建立连接失败" << std::endl;
+			FD_SET(g_clients[i], &fdread);
+		}
+		timeval time{ 1,0 };
+		int ret = select(_sock + 1,&fdread,&fdwrite,&fdexce,&time);
+		if (ret < 0)
+		{
+			std::cout << "select函数发生错误" << std::endl;
 			exit(1);
 		}
-		std::cout << "新客户端加入，IP = " << inet_ntoa(_clientAddr.sin_addr) << std::endl;
-		while (true)
+
+		if (FD_ISSET(_sock, &fdread))
 		{
-			memset(&dh, 0, sizeof(DataHeader));
-			//5 接受客户端的请求
-			int nlen = recv(accsock, (char*)&dh,sizeof(DataHeader) , 0);
-			std::cout << "debug:接受到的指令: " << dh.cmd 
-				 << "长度:" << dh.dataLength << std::endl;
-			if (nlen <= 0)
+			FD_CLR(_sock,&fdread);
+			accsock = accept(_sock, (sockaddr*)&_clientAddr, &len);
+			if (INVALID_SOCKET == accsock)
 			{
-				std::cout << "客户端关闭" << std::endl;
-				break;
+				std::cout << "建立连接失败" << std::endl;
+				exit(1);
 			}
-			switch (dh.cmd)
-			{
-			case CMD_LOGIN:
-			{
-				Login login{};
-				recv(accsock, (char*)&login + leng, sizeof(Login) - leng, 0);
-				
-				std::cout << "用户名：" << login.userName << "\n"
-					<< "密码：" << login.passWord << std::endl;
+			std::cout << "新客户端加入，IP = " << inet_ntoa(_clientAddr.sin_addr) << std::endl;
+			g_clients.push_back(accsock);
+		}
 
-				LoginResult logret{};
-				send(accsock, (char*)&logret, sizeof(logret),0);
-			}
-			break;
-			case CMD_LOGOUT:
+		for (size_t i = 0; i < fdread.fd_count; i++)
+		{
+			if (-1 == processor(fdread.fd_array[i]))
 			{
-				LogOut logout{};
-				recv(accsock, (char*)&logout +leng, sizeof(LogOut) - leng, 0);
-
-				std::cout << "用户 " << logout.userName << " 登出" << std::endl;
-				//忽略判断用户名密码
-				LogOutResult logret{  };
-				send(accsock, (char*)&logret, sizeof(logret), 0);
-			}
-			break;
-			default:
-				dh.cmd = CMD_ERROR;
-				dh.dataLength = 0;
-				std::cout << "不能解析的指令" << std::endl;
-				break;
+				auto iter = find(g_clients.begin(), g_clients.end(), fdread.fd_array[i]);
+				if (iter != g_clients.end())
+				{
+					g_clients.erase(iter);
+				}
 			}
 		}
-		closesocket(accsock);
+
+		std::cout << "空闲时间处理其他业务" << std::endl;
+	}
+	for (size_t i = 0;i <  g_clients.size(); i++)
+	{
+		FD_SET(g_clients[i], &fdread);
+		closesocket(g_clients[i]);
 	}
 	//	6. 关闭 socket  ——closesocket
 	closesocket(_sock);
